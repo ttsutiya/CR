3e62d09e9e6242bfafccb3fab5733d074debe964
Tigger <thiago.tsutiya@gmail.com>
2021-01-20 20:45:49 -0300

    included 'else' in the run.sh file

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ if ! g++ Main.cc -o CR
 then
     echo "Compiling error exiting script"
     exit;
-fi
-
+else
     ./CR
     root PlotPosition.cc #PlotMagField.cc PlotFrequency.cc PlotRadius.cc
 #    root  PlotX.cc PlotY.cc PlotZ.cc
+fi
+
