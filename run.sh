63937cb852be037a86bc9bf888257606b1fc1360
Tigger <thiago.tsutiya@gmail.com>
2021-01-20 21:14:13 -0300

    added nested if in run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,12 @@ then
     echo "Compiling error exiting script"
     exit;
 else
-    ./CR
-    root PlotPosition.cc #PlotMagField.cc PlotFrequency.cc PlotRadius.cc
-#    root  PlotX.cc PlotY.cc PlotZ.cc
+    if ./CR
+    then
+        echo "Running ROOT"
+        #root PlotPosition.cc #PlotMagField.cc PlotFrequency.cc PlotRadius.cc
+        #root  PlotX.cc PlotY.cc PlotZ.cc
+    fi
+
 fi
 
