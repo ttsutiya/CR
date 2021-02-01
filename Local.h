f93236efc4adeb12a1bf359410569863312ad37a
Tigger <thiago.tsutiya@gmail.com>
2021-02-01 13:32:54 -0300

    Rk4.h added
     this header will deal with any function related to the rk4 method

--- Local.h
+++ Local.h
@@ -1,3 +1,4 @@
 #include "Mathematics.h"
 #include "Physics.h"
 #include "Print.h"
+#include "Rk4.h"
--- Main.cc
+++ Main.cc
@@ -60,74 +60,17 @@ int main(){
     frequency[0] = Frequency(q,m,v,B);
 
 //#############################################################################
-//                               RK4
-//#############################################################################
-    int adap = 0;       //adaptative counter
 
     for(double time = h; time <= finalTime;){         
 
-        std::vector<double> F(3);
-        std::vector<double> x1(3),x2(3),x3(3), x4(3);
-        std::vector<double> v1(3),v2(3),v3(3), v4(3);
-        std::vector<double> a1(3),a2(3),a3(3), a4(3);
-
-        for(int i=0; i<3; i++){
-            x1[i] = pos[i];
-            v1[i] = v[i];
-        }
-
-        MagneticField(x1,B,B0,initialB,mode);
-        Lorentz(F,q,v1,B);
-
-        for(int i=0; i<3; i++){
-            a1[i] = F[i]/m;
-        }
-
-        for(int i=0; i<3; i++){
-            x2[i] = pos[i] + 0.5 * v1[i] * h;
-            v2[i] = v[i] + 0.5 * a1[i] * h;
-        }
-
-        MagneticField(x2,B,B0,initialB,mode);
-        Lorentz(F,q,v2,B);
+        Rk4(pos,v,B,B0,initialB,mode,q,m,h);
         
-        for(int i=0; i<3; i++){
-            a2[i] = F[i]/m;
-        }
-
-        for(int i=0; i<3; i++){
-            x3[i] = pos[i] + 0.5 * v2[i] * h;
-            v3[i] = v[i] + 0.5 * a2[i] * h;
-        }
-
-        MagneticField(x3,B,B0,initialB,mode);
-        Lorentz(F,q,v3,B);
-        
-        for(int i=0; i<3; i++){
-            a3[i] = F[i]/m;
-        }
-
-        for(int i=0; i<3; i++){
-            x4[i] = pos[i] + v3[i] * h;
-            v4[i] = v[i] + a3[i] * h;
-        }
-
-        MagneticField(x4,B,B0,initialB,mode);
-        Lorentz(F,q,v4,B);
-        
-        for(int i=0; i<3; i++){
-            a4[i] = F[i]/m;
-        }
-
-        for(int i=0; i<3; i++){
-            pos[i] += (h/6) * (v1[i] + 2*v2[i] + 2*v3[i] + v4[i]);
-        }
-
-        for(int i=0; i<3; i++){
-            v[i] += (h/6) * (a1[i] + 2*a2[i] + 2*a3[i] + a4[i]);
-        }
-
-//        if(adap == 0) 
+//        if(adapCounter == 0){
+//            adapCounter++;
+//        }else if(adapCounter == 1){
+//
+//            adapCounter = 0;
+//        }
 
         std::vector<double> temp(3);    //temporary vec to be used to push
                                         //into vector of vectors
